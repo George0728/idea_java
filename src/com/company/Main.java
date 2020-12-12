773905c90faa991f0b01259dacc0fcb6023b2dbe
George0728 <chengaopan0728@163.com>
2020-12-13 01:22:46 +0800

    java project(idea)

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("my first coding language");
+        int[] a = new int[5];
+        a[0] = 1;
+        a[2] = 3;
+        for(int i : a){
+            System.out.println(i);
+        }
+    }
+}
